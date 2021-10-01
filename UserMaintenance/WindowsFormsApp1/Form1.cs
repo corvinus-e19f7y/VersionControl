e408f34e281d2091780d8430bae941e4789ac763
Botond Kovács <botond.kovacs2@stud.uni-corvinus.hu>
2021-10-02 00:02:18 +0200

    delete function

--- UserMaintenance/WindowsFormsApp1/Form1.cs
+++ UserMaintenance/WindowsFormsApp1/Form1.cs
@@ -22,6 +22,7 @@ namespace WindowsFormsApp1
             label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
             button2.Text = Resource1.WriteToFile;
+            button3.Text = Resource1.Delete;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -51,6 +52,15 @@ namespace WindowsFormsApp1
                         u.ID,
                         u.FullName));
                 }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var törlés = listBox1.SelectedItem;
+            if (törlés != null)
+            {
+                users.Remove((User)törlés);
             }
         }
     }
