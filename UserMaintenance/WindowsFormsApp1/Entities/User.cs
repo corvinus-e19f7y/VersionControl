acd0e0c812552ac544791b0d31216ecbc9b5a1bd
Botond Kovács <botond.kovacs2@stud.uni-corvinus.hu>
2021-10-01 10:42:13 +0200

    Create default UI

---
+++ UserMaintenance/WindowsFormsApp1/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Entities
+{
+     public  class User
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        private int myVar;
+
+        public string FullName
+        {
+            get 
+            {
+                return string.Format(
+                    "{0} {1}",
+                    LastName,
+                    FullName); 
+            }
+        }
+
+    }
+}
