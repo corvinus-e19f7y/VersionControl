52dca36a729cec1bd105b8235b2a21dd5b8de70f
Botond Kovács <botond.kovacs2@stud.uni-corvinus.hu>
2021-10-22 10:57:48 +0200

    add getportfoliovalue

--- VaR/VaR/Form1.cs
+++ VaR/VaR/Form1.cs
@@ -21,9 +21,9 @@ namespace VaR
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
-            dataGridView1.DataSource = context.Ticks;
+            dataGridView1.DataSource = Ticks;
             CreatePortfolio();
-
+  
         }
 
         private void CreatePortfolio()
@@ -33,6 +33,21 @@ namespace VaR
             Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
 
             dataGridView2.DataSource = Portfolio;
+        }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
         }
     }
 }
